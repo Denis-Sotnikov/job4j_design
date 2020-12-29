e92021f7e4dad8afe083a61c96a7b30783a1cb74
Denis Sotnikov <sport_piter@mail.ru>
2020-12-29 16:45:00 +0300

    1. Реализовать коллекцию Set на массиве [#437771]

---
+++ src/main/java/ru/job4j/collection/SimpleSet.java
@@ -0,0 +1,45 @@
+package ru.job4j.collection;
+
+import java.util.Iterator;
+
+public class SimpleSet<T> implements Iterable {
+    private SimpleArray<T> array = new SimpleArray<>();
+
+    public boolean add(T element) {
+        Iterator<T> it = array.iterator();
+        while (it.hasNext()) {
+            if (it.next().equals(element)) {
+                return false;
+            }
+        }
+        array.add(element);
+        return true;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return array.iterator();
+    }
+
+    public static void main(String[] args) {
+        SimpleSet<Integer> array = new SimpleSet<>();
+        array.add(5);
+        array.add(5);
+//        array.add(15);
+//        array.add(20);
+//        array.add(10);
+        Iterator<Integer> flag = array.iterator();
+        System.out.println(flag.hasNext());
+        System.out.println(flag.next());
+        System.out.println(flag.hasNext());
+        System.out.println(flag.next());
+        System.out.println(flag.hasNext());
+        System.out.println(flag.next());
+        System.out.println(flag.hasNext());
+        System.out.println(flag.next());
+        System.out.println(flag.hasNext());
+        System.out.println(flag.next());
+
+    }
+
+}
