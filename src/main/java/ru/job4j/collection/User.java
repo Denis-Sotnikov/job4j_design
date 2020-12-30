d269478d4c1f4b1af95789dc48037e90189973c9
Denis Sotnikov <sport_piter@mail.ru>
2020-12-30 16:15:14 +0300

    1. Создать модель User [#437774]

--- src/main/java/ru/job4j/collection/User.java
+++ src/main/java/ru/job4j/collection/User.java
@@ -1,9 +1,65 @@
 package ru.job4j.collection;
 
-import java.util.Calendar;
+import java.util.*;
 
 public class User {
+
     private String name;
     private int children;
     private Calendar birthday;
+
+    public User() {
+    }
+
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getChildren() {
+        return children;
+    }
+
+    public void setChildren(int children) {
+        this.children = children;
+    }
+
+    public Calendar getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Calendar birthday) {
+        this.birthday = birthday;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return children == user.children &&
+                Objects.equals(name, user.name) &&
+                Objects.equals(birthday, user.birthday);
+    }
+
+    public static void main(String[] args) {
+        User first = new User("Dmitriy", 1, new GregorianCalendar(2017, 0, 25));
+        User second = new User("Dmitriy", 1, new GregorianCalendar(2017, 0, 25));
+        Map<User, Object> userMap = new HashMap<>();
+        userMap.put(first, new Object());
+        userMap.put(second, new Object());
+        System.out.println(userMap);
+        for (Map.Entry<User, Object> t : userMap.entrySet()) {
+            System.out.println(t);
+        }
+    }
 }
