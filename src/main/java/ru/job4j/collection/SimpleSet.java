f0a0a19a385f6bb7dca65c97a9d222676a12e09c
Denis Sotnikov <sport_piter@mail.ru>
2021-01-02 03:35:06 +0300

    1. Реализовать коллекцию Set на массиве [#437771]

--- src/main/java/ru/job4j/collection/SimpleSet.java
+++ src/main/java/ru/job4j/collection/SimpleSet.java
@@ -1,18 +1,26 @@
 package ru.job4j.collection;
 
 import java.util.Iterator;
+import java.util.Objects;
 
 public class SimpleSet<T> implements Iterable {
     private SimpleArray<T> array = new SimpleArray<>();
 
     public boolean add(T element) {
+        if (check(element)) {
+            array.add(element);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean check(T element) {
         Iterator<T> it = array.iterator();
         while (it.hasNext()) {
-            if (it.next().equals(element)) {
+            if (Objects.equals(it.next(), element)) {
                 return false;
             }
         }
-        array.add(element);
         return true;
     }
 
@@ -23,16 +31,17 @@ public class SimpleSet<T> implements Iterable {
 
     public static void main(String[] args) {
         SimpleSet<Integer> array = new SimpleSet<>();
+        array.add(null);
+        array.add(null);
         array.add(5);
-        array.add(5);
-//        array.add(15);
-//        array.add(20);
-//        array.add(10);
+        array.add(15);
+        array.add(20);
+        array.add(10);
         Iterator<Integer> flag = array.iterator();
         System.out.println(flag.hasNext());
-//        System.out.println(flag.next());
-//        System.out.println(flag.hasNext());
-//        System.out.println(flag.next());
+        System.out.println(flag.next());
+        System.out.println(flag.hasNext());
+        System.out.println(flag.next());
 //        System.out.println(flag.hasNext());
 //        System.out.println(flag.next());
 //        System.out.println(flag.hasNext());
