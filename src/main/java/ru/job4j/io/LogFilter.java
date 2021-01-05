2ab6d33df07ae8ebaba3e461dd10a4e2fa2899ad
Denis Sotnikov <sport_piter@mail.ru>
2021-01-06 02:59:47 +0300

    0.3. BufferedReader. [#437795]

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -2,30 +2,20 @@ package ru.job4j.io;
 
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class LogFilter {
     public static List<String> filter(String file) {
-        List<String> arrayForReturn = new ArrayList<String>();
-//        List<String> forReturn = new ArrayList<String>();
+        List<String> forReturn = new ArrayList<String>();
         try (BufferedReader in = new BufferedReader(new FileReader("log.txt"))) {
-            List<String> array = new ArrayList<String>();
-//            forReturn = in.lines()
-//                    .filter(s->s.split(" ")[s.split(" ").length-2].equals("404"))
-//                    .collect(Collectors.toList());
-            in.lines().forEach(array::add);
-            for (String line : array) {
-                String[] box = line.split(" ");
-                if (box[box.length - 2].equals("404")) {
-                    arrayForReturn.add(line);
-                }
-            }
+            forReturn = in.lines()
+                    .filter(s -> s.split(" ")[s.split(" ").length - 2].equals("404"))
+                    .collect(Collectors.toList());
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return arrayForReturn;
+        return forReturn;
     }
 
     public static void save(List<String> log, String file) {
