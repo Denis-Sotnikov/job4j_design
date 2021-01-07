fd09e8ce5b48adb5ba073a02920c1ff69685b5bb
Denis Sotnikov <sport_piter@mail.ru>
2021-01-07 08:15:47 +0300

    2. Анализ доступности сервера. [#437801]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -9,74 +9,46 @@ public class Analizy {
     private List<String> listUnavailable = new LinkedList<>();
 
     public void unavailable(File source, File target) throws ParseException {
-        anavailable(source, target);
         try (PrintWriter out = new PrintWriter(
                 new BufferedOutputStream(
                         new FileOutputStream(target)
                 ))) {
-            for (int i = 0; i < listUnavailable.size(); i++) {
-                if (i % 2 != 0) {
-                    out.write(listUnavailable.get(i));
-                    out.write("\n");
-                    System.out.print(listUnavailable.get(i));
-                    System.out.println();
-                } else {
-                    out.write(listUnavailable.get(i) + " : ");
-                    System.out.print(listUnavailable.get(i) + " : ");
-                }
-            }
+            out.write(anavailable(source, target));
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public void anavailable(File source, File target) throws ParseException {
+    public String anavailable(File source, File target) throws ParseException {
+        StringBuilder builder = new StringBuilder();
+        Boolean availableServer = true;
         try (BufferedReader in = new BufferedReader(new FileReader(source))) {
-            List<String> linesis = new ArrayList<String>();
-            in.lines().forEach(linesis::add);
-            for (String line : linesis) {
-                if (line.contains(":")) {
-                    listStatus.add(line);
-                    String[] array = line.split(" ");
-                    String value = array[0];
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        for (String w : this.listStatus) {
-            String[] array = w.split(" ");
-            if (Integer.parseInt(array[0]) == 400 || Integer.parseInt(array[0]) == 500) {
-                if (listUnavailable.size() % 2 == 0) {
-                    listUnavailable.add(array[1]);
-                }
-            } else {
-                if (Integer.parseInt(array[0]) == 200 || Integer.parseInt(array[0]) == 300) {
-                    if (listUnavailable.size() % 2 != 0) {
-                        listUnavailable.add(array[1]);
+            while (in.ready()) {
+                String value = in.readLine();
+                if (availableServer) {
+                    if (value.contains("400") || value.contains("500")) {
+                        availableServer = false;
+                        String[] array = value.split(" ");
+                        builder.append(array[1]);
+                        builder.append(" : ");
+                    }
+                } else {
+                    if (value.contains("200") || value.contains("300")) {
+                        availableServer = true;
+                        String[] array = value.split(" ");
+                        builder.append(array[1]);
+                        builder.append("\n");
                     }
                 }
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+return builder.toString();
     }
 
     public static void main(String[] args) throws ParseException {
-//        Analizy analizy = new Analizy();
-//        analizy.unavailable("server.log", "resultAnalizy.log");
-////        analizy.anavailable("server.log", "resultAnalizy");
-//        for (String w : analizy.listStatus) {
-//            System.out.println(w);
-//        }
-//
-//        for (String w : analizy.listUnavailable) {
-//            System.out.println(w);
-//        }
-//        Date t = new Date();
-//        DateFormat df = new SimpleDateFormat("HH:mm:ss");
-//        t = df.parse("15:08:10");
-//        SimpleDateFormat time = new SimpleDateFormat("HH:mm:ss", Locale.ENGLISH);
-//        System.out.println("hare = " + time.format(t));
-//        System.out.println(df.format(t));
-
+        Analizy analizy = new Analizy();
+        analizy.unavailable(new File("server.log"), new File("resultAnalizy.log"));
     }
 }
