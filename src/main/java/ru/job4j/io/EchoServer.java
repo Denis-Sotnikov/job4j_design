3001ac377bf9c65d36ce15cdb632356fd93c5a6c
Denis Sotnikov <sport_piter@mail.ru>
2021-01-09 09:29:55 +0300

    1. Бот [#437817]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -8,6 +8,7 @@ import java.net.Socket;
 public class EchoServer {
     public static void main(String[] args) throws IOException {
         boolean flag = true;
+        int sign = 0;
         try (ServerSocket server = new ServerSocket(9000)) {
             while (flag) {
                 Socket socket = server.accept();
@@ -18,11 +19,22 @@ public class EchoServer {
                     while ((in.ready())) {
                         str = in.readLine();
                         System.out.println(str);
-                        if (str.contains("?msg=Bye")) {
+                        if (str.contains("?msg=Exit")) {
                             flag = false;
                         }
+                        if (str.contains("?msg=Hello")) {
+                            out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                            out.write("Hello, dear friend. \r\n".getBytes());
+                        } else {
+                            if (str.contains("?msg=")) {
+                                out.write("HTTP/1.1 200 OK\r\n".getBytes());
+                                System.out.println(str.split("msg=")[1]);
+                                out.write((str.split("msg=")[1].split(" ")[0] + "\r\n").getBytes());
+                            }
+                        }
                     }
-                    out.write("HTTP/1.1 200 OK\r\n\\".getBytes());
+                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                   out.write("Hello, dear friend.".getBytes());
                 }
             }
         }
