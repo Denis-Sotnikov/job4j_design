5ac966dfaa0989e882158b879e8e3bbe9a545c55
Denis Sotnikov <sport_piter@mail.ru>
2021-01-13 09:11:00 +0300

    2. Статистику по коллекции. [#437739]

--- src/main/java/ru/job4j/collection/Analize.java
+++ src/main/java/ru/job4j/collection/Analize.java
@@ -8,28 +8,21 @@ import java.util.stream.Collectors;
 
 public class Analize {
     public Info diff(List<User> previous, List<User> current) {
+        Map<Integer, User> mapaForUser = new HashMap<>();
         Info info = new Info();
-        Map<Integer, User> mapaForUserPrevious = previous.stream()
-                .collect(Collectors.toMap(k -> k.id, k -> k));
-        Map<Integer, User> mapaForUserCurrent = current.stream()
-                .collect(Collectors.toMap(k -> k.id, v -> v));
-
-        for (User s : current) {
-            if (!mapaForUserPrevious.containsKey(s.id)) {
-                info.added++;
-            }
+        for (User q : current) {
+            mapaForUser.put(q.id, q);
         }
-
-        for (User s : previous) {
-            if (mapaForUserCurrent.containsKey(s.id)) {
-                System.out.println(s.id + " " + s.name);
-                if (!s.name.equals(mapaForUserCurrent.get(s.id).name)) {
+        for (User q : previous) {
+            if (mapaForUser.containsKey(q.id)) {
+                if (!mapaForUser.get(q.id).name.equals(q.name)) {
                     info.changed++;
                 }
             } else {
                 info.deleted++;
             }
         }
+        info.added = current.size() - (previous.size() - info.deleted);
         return info;
     }
 
@@ -77,25 +70,25 @@ public class Analize {
     }
 
     public static void main(String[] args) {
-//        List<User> first = new ArrayList<>();
-//        first.add(new User(1,"Sergey"));
-//        first.add(new User(2,"Vadim"));
-//        first.add(new User(3,"Denis"));
-//        first.add(new User(4,"Vika"));
-//        first.add(new User(5,"Olga"));
-//        first.add(new User(6,"Sasha"));
-//        first.add(new User(7,"Nastya"));
-//        first.add(new User(8,"Stasya"));
-//        List<User> second = new ArrayList<>();
-//        second.add(new User(1,"Sergey"));
-//        second.add(new User(2,"Nika"));
-//        second.add(new User(3,"Denis"));
-//        second.add(new User(4,"Vika"));
-//        second.add(new User(5,"Francheska"));
-//        second.add(new User(6,"Sasha"));
-//        second.add(new User(7,"Nastya"));
-//        second.add(new User(10,"Polina"));
-//        Info info = new Analize().diff(first, second);
-//        System.out.println(info);
+        List<User> first = new ArrayList<>();
+        first.add(new User(1, "Sergey"));
+        first.add(new User(2, "Vadim"));
+        first.add(new User(3, "Denis"));
+        first.add(new User(4, "Vika"));
+        first.add(new User(5, "Olga"));
+        first.add(new User(6, "Sasha"));
+        first.add(new User(7, "Nastya"));
+        first.add(new User(8, "Stasya"));
+        List<User> second = new ArrayList<>();
+        second.add(new User(1, "Sergey"));
+        second.add(new User(2, "Nika"));
+        second.add(new User(3, "Denis"));
+        second.add(new User(4, "Vika"));
+        second.add(new User(5, "Francheska"));
+        second.add(new User(6, "Sasha"));
+        second.add(new User(7, "Nastya"));
+        second.add(new User(10, "Polina"));
+        Info info = new Analize().diff(first, second);
+        System.out.println(info);
     }
 }
