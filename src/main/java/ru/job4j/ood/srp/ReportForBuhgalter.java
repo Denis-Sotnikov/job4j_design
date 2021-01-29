3351a2d2354a7214204f018d89efef2ab3835917
Denis Sotnikov <sport_piter@mail.ru>
2021-01-29 23:47:26 +0300

    1. Отчеты. [#437833]

--- src/main/java/ru/job4j/ood/srp/ReportForBuhgalter.java
+++ src/main/java/ru/job4j/ood/srp/ReportForBuhgalter.java
@@ -1,28 +1,11 @@
 package ru.job4j.ood.srp;
 
 import java.util.Comparator;
-import java.util.List;
 import java.util.function.Predicate;
 
-public class ReportEngine {
-    private Report report;
-
-    private Store store;
-
-    public ReportEngine(Store store) {
-        this.store = store;
-    }
-
-    public ReportEngine(Store store, Report report) {
-        this.report = report;
-        this.store = store;
-    }
-
-    public Report getReport() {
-        return report;
-    }
-
-    public String generate(Predicate<Employee> filter) {
+public class ReportForBuhgalter implements Report {
+    @Override
+    public String generate(Predicate<Employee> filter, Store store) {
         StringBuilder text = new StringBuilder();
         text.append("Name; Hired; Fired; Salary;");
         text.append(System.lineSeparator());
@@ -30,7 +13,7 @@ public class ReportEngine {
             text.append(employee.getName()).append(";")
                     .append(employee.getHired()).append(";")
                     .append(employee.getFired()).append(";")
-                    .append(employee.getSalary()).append(";")
+                    .append(employee.getSalary() * 2).append(";")
                     .append(System.lineSeparator());
         }
         return text.toString();
