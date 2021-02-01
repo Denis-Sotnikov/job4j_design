c4cb4fc7c5cfadf0f491360b240ac796a14de850
Denis Sotnikov <sport_piter@mail.ru>
2021-02-01 15:32:05 +0300

    travisBug

--- src/main/java/ru/job4j/ood/lsp/CalculationFermaInAnotherRegion.java
+++
@@ -1,7 +0,0 @@
-package ru.job4j.ood.lsp;
-// Смотрите класс CalculationFerma
-public class CalculationFermaInAnotherRegion extends CalculationFerma {
-    private boolean calculation() {
-        return g < ((N / 2) * A * l * (l / 2));
-    }
-}
---
+++ src/main/java/ru/job4j/ood/lsp/FermaInAnotherRegion.java
@@ -0,0 +1,7 @@
+package ru.job4j.ood.lsp;
+
+public class FermaInAnotherRegion extends CalculationFerma {
+    private boolean calculation() {
+        return 8 < ((7 / 2) * 15 * 15 * (10 / 2));
+    }
+}
