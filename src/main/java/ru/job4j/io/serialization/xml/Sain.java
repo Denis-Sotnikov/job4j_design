e715bd3f0b4f96642f12ac5a9d720f0068590dff
Denis Sotnikov <sport_piter@mail.ru>
2021-02-02 06:55:32 +0300

    bug

--- src/main/java/ru/job4j/io/serialization/xml/Sain.java
+++ src/main/java/ru/job4j/io/serialization/xml/Sain.java
@@ -10,25 +10,25 @@ import java.io.StringReader;
 import java.io.StringWriter;
 
 public class Sain {
-    public static void main(String[] args) throws Exception {
-        Home home = new Home(
-                "new Earth", 2, true, new IngenerSolutions(
-                "First", "Second"), new String[]{"First", "Second"});
-
-        JAXBContext context = JAXBContext.newInstance(Home.class);
-        Marshaller marshaller = context.createMarshaller();
-        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-        String xml = "";
-        try (StringWriter writer = new StringWriter()) {
-            marshaller.marshal(home, writer);
-            xml = writer.getBuffer().toString();
-            System.out.println(xml);
-        }
-        Unmarshaller unmarshaller = context.createUnmarshaller();
-        try (StringReader reader = new StringReader(xml)) {
-
-            Home result = (Home) unmarshaller.unmarshal(reader);
-            System.out.println(result);
-        }
-    }
+//    public static void main(String[] args) throws Exception {
+//        Home home = new Home(
+//                "new Earth", 2, true, new IngenerSolutions(
+//                "First", "Second"), new String[]{"First", "Second"});
+//
+//        JAXBContext context = JAXBContext.newInstance(Home.class);
+//        Marshaller marshaller = context.createMarshaller();
+//        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+//        String xml = "";
+//        try (StringWriter writer = new StringWriter()) {
+//            marshaller.marshal(home, writer);
+//            xml = writer.getBuffer().toString();
+//            System.out.println(xml);
+//        }
+//        Unmarshaller unmarshaller = context.createUnmarshaller();
+//        try (StringReader reader = new StringReader(xml)) {
+//
+//            Home result = (Home) unmarshaller.unmarshal(reader);
+//            System.out.println(result);
+//        }
+//    }
 }
